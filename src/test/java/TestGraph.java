2e843cca7b70560c99f87e184d2bef00c3fd135d
fabienogli <fabien.ogli@etu.univ-lyon1.fr>
2018-03-04 17:31:01 +0100

    add spaces

--- src/test/java/TestGraph.java
+++ src/test/java/TestGraph.java
@@ -21,8 +21,15 @@ public class TestGraph {
 		Arc a3 = new Arc(n1,n3, null);
 
 		IDirectedGraph g = new Graph();
-		g.addNode(n1);g.addNode(n2);g.addNode(n3);g.addNode(n4);
-		g.addArc(a1);g.addArc(a1reversed);g.addArc(a2);g.addArc(a2reversed);g.addArc(a3);
+		g.addNode(n1);
+		g.addNode(n2);
+		g.addNode(n3);
+		g.addNode(n4);
+		g.addArc(a1);
+		g.addArc(a1reversed);
+		g.addArc(a2);
+		g.addArc(a2reversed);
+		g.addArc(a3);
 		System.out.println(g);
 		System.out.println(g.hasArc(n1, n2)+" "+g.hasArc(n2, n4));
 		System.out.println(g.getAdjNodes(n3));
